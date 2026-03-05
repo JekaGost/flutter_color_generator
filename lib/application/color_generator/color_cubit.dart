5490052c8c7c91ad2f5340369dbe7d140f971f39
Ievgenii Gostyshchev <jekagost@gmail.com>
2026-03-05 16:19:47 +0300

    random color logic added

--- lib/application/color_generator/color_cubit.dart
+++ lib/application/color_generator/color_cubit.dart
@@ -1,1 +1,20 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'color_state.dart';
 
+class ColorCubit extends Cubit<ColorState> {
+  ColorCubit()
+    : super(const ColorState(backgroundColor: Colors.lightGreenAccent));
+
+  void changeColor() {
+    final random = Random();
+
+    final int r = random.nextInt(256);
+    final int g = random.nextInt(256);
+    final int b = random.nextInt(256);
+
+    final Color newColor = Color.fromARGB(255, r, g, b);
+    emit(state.copyWith(backgroundColor: newColor));
+  }
+}
