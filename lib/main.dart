b422fe133eaf13b77218ff75b8d865d20ff62f15
Ievgenii Gostyshchev <jekagost@gmail.com>
2026-03-05 16:27:09 +0300

    main wrapped into BlockProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'application/color_generator/color_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => ColorCubit(),
+      child: MaterialApp(
+        title: 'Color Generator',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
       ),
     );
   }
